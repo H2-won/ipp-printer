8580062a6dcf3c83d96fc85628040aada045ccf0
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2021-07-05 00:09:00 +0900

    테스트 우선 개발 예시 [21.07.05]
    with faker

--- testIndex.test.js
+++ testIndex.test.js
@@ -1,4 +1,5 @@
 const sut = require('./testIndex');
+const faker = require('faker');
 
 // test('sut correctly works', () => {
 //   for (const source of ['hello  world', 'hello   world', 'hello    world']) {
@@ -8,11 +9,43 @@ const sut = require('./testIndex');
 // });
 
 test.each`
-  source              | expected
-  ${'hello  world'}   | ${'hello world'}
-  ${'hello   world'}  | ${'hello world'}
-  ${'hello    world'} | ${'hello world'}
+  source                 | expected
+  ${'hello  world'}      | ${'hello world'}
+  ${'hello   world'}     | ${'hello world'}
+  ${'hello    world'}    | ${'hello world'}
+  ${'hello     world'}   | ${'hello world'}
+  ${'hello      world'}  | ${'hello world'}
+  ${'hello       world'} | ${'hello world'}
 `('sut transforms "$source" to "$expected"', ({ source, expected }) => {
   const actual = sut(source);
   expect(actual).toBe(expected);
+});
+
+test.each`
+  source             | expected
+  ${'hello\t world'} | ${'hello world'}
+  ${'hello \tworld'} | ${'hello world'}
+`('sut transforms "$source" that contains tab character to "$expected"', ({ source, expected }) => {
+  const actual = sut(source);
+  expect(actual).toBe(expected);
+});
+
+test.each`
+  source             | bannedWords              | expected
+  ${'hello mockist'} | ${['mockist', 'purist']} | ${'hello *******'}
+  ${'hello purist'}  | ${['mockist', 'purist']} | ${'hello ******'}
+`('sut transforms "$source" to "$expected"', ({ source, bannedWords, expected }) => {
+  const actual = sut(source, { bannedWords });
+  expect(actual).toBe(expected);
+});
+
+describe('given banned word', () => {
+  const bannedWord = faker.lorem.word();
+  const source = 'hello ' + bannedWord;
+  const expected = 'hello ' + '*'.repeat(bannedWord.length);
+
+  test(`${bannedWord} when invoke sut then it returns ${expected}`, () => {
+    const actual = sut(source, { bannedWords: [bannedWord] });
+    expect(actual).toBe(expected);
+  });
 });
